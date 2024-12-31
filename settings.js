14a1ed998402d37fc9fbcc4d44ffc569be97a6d7
pandaliciousxd <167933198+pandaliciousxd@users.noreply.github.com>
2025-01-01 00:36:07 +0300

    Update settings.js

--- settings.js
+++ settings.js
@@ -43,6 +43,6 @@ function convertToBool(text, fault = 'true') {
     return text === fault ? true : false;
 }
 module.exports = {
-SESSION_ID: process.env.SESSION_ID || "Manu-MD&SZFgxKAR#6_NyKpEpowTYlvcmjcQkc4cAaDdYLM0YAn6AHB7AKBQ", // ඔයාගෙ සෙසන් එක දාගන්න / Put Your Session_id Here !!! 💚
-MONGODB: process.env.MONGODB || "mongodb+srv://manulwijethilaka42:manumd123@cluster0.le0te.mongodb.net/", // ඔයාගෙ Mongodb Url එක දාගන්න / Put Your Mongodb Url Here !!! 💚
+SESSION_ID: process.env.SESSION_ID || "Manu-MD&ac1XFZDI#-Oujm1kes_gWvC5XCiAEPaXlZWNEyQAVs9uhHbkH6rU", // ඔයාගෙ සෙසන් එක දාගන්න / Put Your Session_id Here !!! 💚
+MONGODB: process.env.MONGODB || "mongodb+srv://keboyeh916:w3b8c07bBUgArIl4@cluster1.viqaz.mongodb.net/?retryWrites=true&w=majority&appName=Cluster1", // ඔයාගෙ Mongodb Url එක දාගන්න / Put Your Mongodb Url Here !!! 💚
 };
